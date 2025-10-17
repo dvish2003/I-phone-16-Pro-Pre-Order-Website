20b4b436cf41b5b35ecad8455ca3f7724d2cc3b6
dvish2003 <vishanchathuranga63@gmail.com>
2025-10-17 19:30:18 +0530

    Auto mated commit

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,7 @@
 # Git push script
 git add --all
 git commit -m "Auto mated commit"
-git push origin main-dev
+git push origin main
 
 # Note: The lines with "--- IGNORE ---" indicate sections that were removed and should not be included in the final script.
 
